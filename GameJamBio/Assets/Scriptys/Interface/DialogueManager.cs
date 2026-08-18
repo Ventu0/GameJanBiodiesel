993ce4c5ff81a7f50e6749d6085e64d9bdb098b8
victoriaLM230 <victoria.martins1@alunos.sc.senac.br>
2026-08-18 13:54:18 -0300

    Revert "Merge branch 'main' of https://github.com/Ventu0/GameJanBiodiesel"
    This reverts commit 1c14d51bc47eab25052ae8655175af9a94df84c2, reversing
    changes made to 9f865c1b63c11c7dee1b0b78e5cd292d9a22e226.

--- GameJamBio/Assets/Scriptys/Interface/DialogueManager.cs
+++ GameJamBio/Assets/Scriptys/Interface/DialogueManager.cs
@@ -10,7 +10,7 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI characterName;    
     public TextMeshProUGUI dialogueArea;
 
-    private Queue<DialogueLine> lines = new Queue<DialogueLine>();
+    private Queue<DialogueLine> lines;
 
     public bool isDialogueActive = false;
 
@@ -22,16 +22,13 @@ public class DialogueManager : MonoBehaviour
         {
             instance = this;
         }
-        
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
-       
         isDialogueActive = true;
-        print(lines);
-        if(lines != null && lines.Count > 0) 
-            lines.Clear();
+        
+        lines.Clear();
 
         foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
         {
@@ -41,7 +38,6 @@ public class DialogueManager : MonoBehaviour
         DisplayNextDialogueLine();
     }
 
-    
     public void DisplayNextDialogueLine()
     {
         if (lines.Count == 0)
